dc2f60ce9edec4ca8129443ec362247983389cf5
Daniel Araujo <danielpsd2004@gmail.com>
2023-02-07 23:30:00 -0300

    Mecajato

---
+++ clientes/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Cliente, Carro
+import re
+
+def clientes(request):
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            #TODO: Adicionar mensagens
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'carros': zip(carros, placas, anos) })
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            #TODO: Adicionar mensagens
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'carros': zip(carros, placas, anos)})
+        
+        #Validar CPF
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+        cliente.save()
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            carro = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
+            carro.save()
+
+        #Renderizar template
+        return HttpResponse('teste')
